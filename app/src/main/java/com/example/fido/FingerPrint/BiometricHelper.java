3cdd52d57ec8e2a0396646ed6514a2e48b560489
kddco <52121443+kddco@users.noreply.github.com>
2023-03-24 21:19:36 +0800

    register
    未啟用功能

--- app/src/main/java/com/example/fido/FingerPrint/BiometricHelper.java
+++ app/src/main/java/com/example/fido/FingerPrint/BiometricHelper.java
@@ -1,19 +1,19 @@
 package com.example.fido.FingerPrint;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
+import android.view.View;
 import androidx.annotation.RequiresApi;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
+import com.example.fido.R;
 import com.google.android.material.snackbar.Snackbar;
-
 import java.util.concurrent.Executor;
+
+import com.example.fido.fidoapi.*;
 
 public class BiometricHelper extends FragmentActivity{
     private Context context;
@@ -29,9 +29,13 @@ public class BiometricHelper extends FragmentActivity{
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public void authenticate() {
+    public void authenticate(){
         BiometricManager biometricManager = BiometricManager.from(context);
         int result = biometricManager.canAuthenticate();
+        View view = ((Activity) context).findViewById(R.id.imageView);
+
+
+
         if (result == BiometricManager.BIOMETRIC_SUCCESS) {
             BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
                     .setTitle("Title")
@@ -40,12 +44,15 @@ public class BiometricHelper extends FragmentActivity{
                     .setNegativeButtonText("Cancel")
                     .build();
             biometricPrompt.authenticate(promptInfo);
+            Snackbar.make(view, "PASS", Snackbar.LENGTH_LONG).show();
+
+
         } else if (result == BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE) {
-            Snackbar.make(findViewById(android.R.id.content), "硬體不支援", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(view, "硬體不支援", Snackbar.LENGTH_LONG).show();
         } else if (result == BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE) {
-            Snackbar.make(findViewById(android.R.id.content), "硬體不可用", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(view, "硬體不可用", Snackbar.LENGTH_LONG).show();
         } else if (result == BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED) {
-            Snackbar.make(findViewById(android.R.id.content), "沒有任何註冊指紋", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(view, "沒有任何註冊指紋", Snackbar.LENGTH_LONG).show();
         }
     }
 }
