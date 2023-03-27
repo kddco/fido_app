3e018b30bf4dcdba1f480aabf620afc80055cf75
kddco <52121443+kddco@users.noreply.github.com>
2023-03-27 20:59:58 +0800

    修正gardle錯誤

--- app/src/main/java/com/example/fido/FingerPrint/BiometricHelper.java
+++ app/src/main/java/com/example/fido/FingerPrint/BiometricHelper.java
@@ -4,34 +4,53 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.view.View;
+
 import androidx.annotation.RequiresApi;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+
 import com.example.fido.R;
 import com.google.android.material.snackbar.Snackbar;
+
 import java.util.concurrent.Executor;
 
-public class BiometricHelper extends FragmentActivity{
+public class BiometricHelper {
     private Context context;
     private Executor executor;
     private BiometricPrompt biometricPrompt;
+
+    public interface BiometricCallback {
+        void onBiometricAuthenticationSuccess();
+        void onBiometricAuthenticationError(int errorCode, CharSequence errString);
+    }
 
     public BiometricHelper(Context context) {
         this.context = context;
         executor = ContextCompat.getMainExecutor(context);
         biometricPrompt = new BiometricPrompt((FragmentActivity) context, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                BiometricCallback callback = (BiometricCallback) context;
+                callback.onBiometricAuthenticationSuccess();
+            }
 
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                BiometricCallback callback = (BiometricCallback) context;
+                callback.onBiometricAuthenticationError(errorCode, errString);
+            }
         });
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public boolean authenticate(){
+    public boolean authenticate(BiometricCallback callback){
         BiometricManager biometricManager = BiometricManager.from(context);
         int result = biometricManager.canAuthenticate();
         View view = ((Activity) context).findViewById(R.id.imageView);
-
 
 
         if (result == BiometricManager.BIOMETRIC_SUCCESS) {
@@ -45,7 +64,6 @@ public class BiometricHelper extends FragmentActivity{
             Snackbar.make(view, "PASS", Snackbar.LENGTH_LONG).show();
             return true;
 
-
         } else if (result == BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE) {
             Snackbar.make(view, "硬體不支援", Snackbar.LENGTH_LONG).show();
             return false;
@@ -56,6 +74,6 @@ public class BiometricHelper extends FragmentActivity{
             Snackbar.make(view, "沒有任何註冊指紋", Snackbar.LENGTH_LONG).show();
             return false;
         }
-    return false;
+        return false;
     }
 }
--- app/src/main/java/com/example/fido/MainActivity.java
+++ app/src/main/java/com/example/fido/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.fido;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-
 import android.content.Context;
-import android.database.Observable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fido.FingerPrint.BiometricHelper;
 import com.example.fido.fidoapi.ConnAPI;
@@ -22,65 +20,78 @@ import io.reactivex.rxjava3.core.SingleObserver;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BiometricHelper.BiometricCallback {
+
     private ImageView fingerprint_img;
     private Context context;
-
-    private ConnAPI connAPI;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = MainActivity.this;
-
-
         initViews();
-        initData();
         setListeners();
-        connAPI = new ConnAPI();
-
     }
 
-    private void initViews()
-    {
-        fingerprint_img = (ImageView) findViewById(R.id.imageView);
-
-    }
-
-    private void initData()
-    {
+    private void initViews() {
+        fingerprint_img = findViewById(R.id.imageView);
     }
 
     private void setListeners() {
         fingerprint_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageView t_v = (ImageView)v;
-
-                if(t_v == fingerprint_img) {
-                    context = MainActivity.this;
+                ImageView t_v = (ImageView) v;
+                if (t_v == fingerprint_img) {
                     BiometricHelper helper = new BiometricHelper(context);
-
-                    Snackbar.make(t_v, "Image clicked!", Snackbar.LENGTH_SHORT).show();
-                    boolean callAPI = helper.authenticate();
-                    if(callAPI){
-                        ConnAPI connAPI = new ConnAPI();
-                        EditText editTextName = (EditText)findViewById(R.id.editTextName);
-                        EditText editTextdisplayName = (EditText)findViewById(R.id.editTextdisplayName);
-                        String Name = editTextName.getText().toString();
-                        String displayName = editTextdisplayName.getText().toString();
-                        String result = "";
-                        try {
-                            result = connAPI.start(Name,displayName);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+                    helper.authenticate(MainActivity.this);
                 }
             }
         });
     }
 
+    @Override
+    public void onBiometricAuthenticationSuccess() {
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextdisplayName = findViewById(R.id.editTextdisplayName);
+        String Name = editTextName.getText().toString();
+        String displayName = editTextdisplayName.getText().toString();
+        Single.fromCallable(() -> {
+                    ConnAPI connAPI = new ConnAPI();
+                    String result = "";
+                    try {
+                        result = connAPI.start(Name, displayName);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return result;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        // do nothing
+                    }
+
+                    @Override
+                    public void onSuccess(String result) {
+                        TextView resultText = findViewById(R.id.resultText);
+                        resultText.setText(result);
+                        Snackbar.make(resultText, "PASS", Snackbar.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onBiometricAuthenticationError(int errorCode, CharSequence errString) {
+        Toast.makeText(MainActivity.this, "Error: " + errString, Toast.LENGTH_SHORT).show();
+    }
 }
+
