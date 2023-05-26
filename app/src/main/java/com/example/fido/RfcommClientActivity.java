289b39e639c4e58ff1294775d5bb4e8b34391317
kddco <52121443+kddco@users.noreply.github.com>
2023-05-26 19:55:55 +0800

    RFCOMM CLIENT send data to server
    bt可連線並傳輸資料

--- app/src/main/java/com/example/fido/RfcommClientActivity.java
+++ app/src/main/java/com/example/fido/RfcommClientActivity.java
@@ -1,35 +1,43 @@
 package com.example.fido;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class RfcommClientActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private static final UUID SERVICE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothSocket socket;
     private BluetoothDevice selectedDevice;
+    private List<BluetoothDevice> devices;
 
     private Spinner deviceSpinner;
     private Button connectButton;
+    private TextView msgView;
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +46,11 @@ public class RfcommClientActivity extends AppCompatActivity implements AdapterVi
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        devices = new ArrayList<>(bluetoothAdapter.getBondedDevices()); // 将此行代码放在 onCreate() 方法中
+
         deviceSpinner = findViewById(R.id.deviceSpinner);
         connectButton = findViewById(R.id.connectButton);
-
+        msgView = findViewById(R.id.textView2);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,17 +63,13 @@ public class RfcommClientActivity extends AppCompatActivity implements AdapterVi
 
     private void setupDeviceSpinner() {
         List<String> deviceNames = new ArrayList<>();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (checkSelfPermission(android.Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.BLUETOOTH, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_REQUEST_CODE);
             return;
         }
-        final List<BluetoothDevice> devices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
 
         for (BluetoothDevice device : devices) {
             deviceNames.add(device.getName());
@@ -75,41 +81,62 @@ public class RfcommClientActivity extends AppCompatActivity implements AdapterVi
         deviceSpinner.setOnItemSelectedListener(this);
     }
 
+
     private void connectToServer() {
         if (selectedDevice != null) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        if (ActivityCompat.checkSelfPermission(RfcommClientActivity.this, android.Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: 考慮調用 ActivityCompat#requestPermissions
-                            // 在這裡請求缺失的權限，然後覆寫
-                            // public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // 以處理用戶授予權限的情況。請參考 Android 文檔中的 ActivityCompat#requestPermissions 以獲取更多詳細信息。
-                            return;
+            if (ActivityCompat.checkSelfPermission(RfcommClientActivity.this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(RfcommClientActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(RfcommClientActivity.this,
+                        new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSION_REQUEST_CODE);
+            } else {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Method m = selectedDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
+                            socket = (BluetoothSocket) m.invoke(selectedDevice, 1);
+                            socket.connect();
+
+                            OutputStream outputStream = socket.getOutputStream();
+                            outputStream.write("hello".getBytes());
+                            Log.d("MyTag", "SEND HELLO TO SERVER");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(RfcommClientActivity.this, "Failed to connect to the server", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         }
-                        socket = selectedDevice.createRfcommSocketToServiceRecord(SERVICE_UUID);
-                        socket.connect();
-                        // 在這裡處理連接成功後的操作
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(RfcommClientActivity.this, "Failed to connect to the server", Toast.LENGTH_SHORT).show();
-                            }
-                        });
                     }
-                }
-            }).start();
+                }).start();
+            }
+        } else {
+            Toast.makeText(RfcommClientActivity.this, "No device selected", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        selectedDevice = (BluetoothDevice) parent.getItemAtPosition(position);
+        String deviceName = (String) parent.getItemAtPosition(position);
+        Log.d("MyTag", "Selected Device Name: " + deviceName);
+        selectedDevice = findDeviceByName(deviceName);
+
+        Toast.makeText(this, "Selected Device: " + deviceName, Toast.LENGTH_SHORT).show();
     }
+
+    private BluetoothDevice findDeviceByName(String deviceName) {
+        for (BluetoothDevice device : devices) {
+            if (device.getName() != null && device.getName().equalsIgnoreCase(deviceName)) {
+                return device;
+            }
+        }
+        return null;
+    }
+
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
